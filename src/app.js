725fd345c3712925460154c7f91b1768acbc6a06
sourabh321joshi <sourabh321joshi@gmail.com>
2024-09-22 10:21:06 +0530

    Exploring GET , POST ,DELETE

--- src/app.js
+++ src/app.js
@@ -2,17 +2,28 @@ const express = require("express");
 
 const app = express();
 
-app.use("/" , (req ,res) =>{
-    res.send("sta!!");
+app.use("/user" ,(req ,res) =>{
+    res.send("HAHAHAHAHHAHAHAH");
+})
+
+app.get("/user" , (req ,res) =>{
+    res.send({firstName: "Sourabh" ,lastName :"joshi"})
 });
+
+app.post("/user" , (req ,res) =>{
+    res.send("database updated successfully!!!");
+});
+
+app.delete("/user" ,(req ,res) =>{
+    res.send("deleted from the database");
+})
+
 
 app.use("/test" , (req , res) =>{
     res.send("Testing route is workinggg fine!!!!!");
 });
 
-app.use("/hello" , (req ,res) =>{
-    res.send("hello hello hello !!!");
-});
+
 
 app.listen(3000 , () =>{
     console.log("working");
