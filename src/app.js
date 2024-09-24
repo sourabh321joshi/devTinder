ad133f875f032d236db693b9c1ec386607b85b20
sourabh321joshi <sourabh321joshi@gmail.com>
2024-09-24 11:10:41 +0530

    GET APIs

--- src/app.js
+++ src/app.js
@@ -14,9 +14,41 @@ app.post("/signup" , async (req ,res) => {
      } catch (err) { 
         res.status(400).send("user cannot created please check!!!");
     }
-     
-
 });
+
+app.get("/user" , async (req ,res) => {
+   const userEmail = req.body.email;
+
+   try{
+      const user =  await User.findOne({email : userEmail})
+      res.send(user);
+   }
+
+   // try { 
+   //    const users = await User.find({email : userEmail});
+   //    if(users.length=== 0) {
+   //       res.status(404).send("user not found");
+   //    }
+   //    else{
+   //       res.send(users);
+   //    }
+   // }
+
+   catch {
+      res.status(400).send("something went wrong");
+   }
+})
+
+app.get("/feed" , async (req , res) => {
+
+   try {
+      const users = await User.find({});
+      res.send(users);
+   }
+   catch{
+      res.status(400).send("something went wrong");
+   }
+})
 
 connectDB().then(() => {
     console.log("database connction successfull");
