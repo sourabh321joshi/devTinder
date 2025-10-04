5dec24c4b0018cb7f300ab01894ccd7c32de03ed
sourabh joshi <sourabh321joshi@gmail.com>
2025-10-04 15:26:25 +0530

    websocket

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,11 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
+
+const getSecretRoomId = (userId,targetUserId) => {
+    return crypto.createHash("sha256")
+    .update([userId ,targetUserId].sort().join("_"))
+    .digest("hex");
+}
 
 const initializeSocket =(server) =>{
     const io = socket(server , {
@@ -9,9 +16,21 @@ const initializeSocket =(server) =>{
 });
 
 io.on("connection" ,(socket) => {
-    socket.on("joinChat" , () => {})
+    socket.on("joinChat" , ({firstName , userId ,  targetUserId}) =>{
+        const roomId = getSecretRoomId(userId,targetUserId);
+        socket.join(roomId); 
+    });
 
-    socket.on("sendMessage" , () => {} )
+    socket.on("sendMessage" , ({
+      firstName,
+      lastName,
+      userId,
+      targetUserId,
+      text
+    }) => {
+        const roomId = getSecretRoomId(userId,targetUserId);
+        io.to(roomId).emit("messageReceived",{firstName , text});
+    } )
 
     socket.on("disconnect" , ()=> {})
 })
